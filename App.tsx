91cce25c57774ed5436241dca0fc6dcc99dd389b
juliflorezg <forezjuli08@hotmail.com>
2022-12-08 09:48:53 -0500

    add PositionScreen & FlexScreen components to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { BoxObjectModel } from './src/screens/BoxObjectModelScreen';
-import { DimensionesScreen } from './src/screens/DimensionesScreen';
+import { FlexScreen } from './src/screens/FlexScreen';
+// import { BoxObjectModel } from './src/screens/BoxObjectModelScreen';
+// import { DimensionesScreen } from './src/screens/DimensionesScreen';
+// import { PositionScreen } from './src/screens/PositionScreen';
 // import { ContadorScreen } from './src/screens/ContadorScreen';
 // import { HolaMundoScreen } from './src/screens/HolaMundoScreen';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{ flex: 1, paddingTop: 40, backgroundColor: "#ddd"}}>
-      {/* <HolaMundoScreen />
-      <ContadorScreen /> */}
-      <BoxObjectModel />
-      <DimensionesScreen/>
+    <SafeAreaView style={{ flex: 1, paddingTop: 40, backgroundColor: '#ddd' }}>
+      {/* <HolaMundoScreen /> */}
+      {/* <ContadorScreen /> */}
+      {/* <BoxObjectModel /> */}
+      {/* <DimensionesScreen/> */}
+      {/* <PositionScreen /> */}
+      <FlexScreen />
     </SafeAreaView>
   );
 };
