b0f0d07a482fb3394b76e1bd9ca53b82dc4cadb1
juliflorezg <forezjuli08@hotmail.com>
2022-12-08 12:42:15 -0500

    add base TareaScreen component

---
+++ src/screens/TareaScreen.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+export const TareaScreen = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.cajaMorada} />
+      <View style={styles.cajaNaranja} />
+      <View style={styles.cajaAzul} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#28425b',
+  },
+  cajaMorada: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#5856d6',
+  },
+  cajaNaranja: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#f0a23b',
+  },
+  cajaAzul: {
+    width: 100,
+    height: 100,
+    borderWidth: 10,
+    borderColor: 'white',
+    backgroundColor: '#28c4d9',
+  },
+});
