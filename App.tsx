9212e9e435cfeeebdf41598d5b564a742a32eebc
juliflorezg <forezjuli08@hotmail.com>
2022-12-08 12:43:21 -0500

    add Tareas components to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,6 +1,17 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { FlexScreen } from './src/screens/FlexScreen';
+// import { TareaScreen } from './src/screens/TareaScreen';
+// import { Tarea1Screen } from './src/screens/Tarea1Screen';
+// import { Tarea2Screen } from './src/screens/Tarea2Screen';
+// import { Tarea3Screen } from './src/screens/Tarea3Screen';
+// import { Tarea4Screen } from './src/screens/Tarea4Screen';
+// import { Tarea5Screen } from './src/screens/Tarea5';
+// import { Tarea6Screen } from './src/screens/Tarea6Screen';
+// import { Tarea7Screen } from './src/screens/Tarea7Screen';
+// import { Tarea8Screen } from './src/screens/Tarea8Screen';
+// import { Tarea9Screen } from './src/screens/Tarea9Screen';
+import { Tarea10Screen } from './src/screens/Tarea10Screen';
+// import { FlexScreen } from './src/screens/FlexScreen';
 // import { BoxObjectModel } from './src/screens/BoxObjectModelScreen';
 // import { DimensionesScreen } from './src/screens/DimensionesScreen';
 // import { PositionScreen } from './src/screens/PositionScreen';
@@ -15,7 +26,18 @@ export const App = () => {
       {/* <BoxObjectModel /> */}
       {/* <DimensionesScreen/> */}
       {/* <PositionScreen /> */}
-      <FlexScreen />
+      {/* <FlexScreen /> */}
+      {/* <TareaScreen /> */}
+      {/* <Tarea1Screen /> */}
+      {/* <Tarea2Screen /> */}
+      {/* <Tarea3Screen /> */}
+      {/* <Tarea4Screen /> */}
+      {/* <Tarea5Screen /> */}
+      {/* <Tarea6Screen /> */}
+      {/* <Tarea7Screen /> */}
+      {/* <Tarea8Screen /> */}
+      {/* <Tarea9Screen /> */}
+      <Tarea10Screen />
     </SafeAreaView>
   );
 };
